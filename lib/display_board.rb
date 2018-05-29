bb09dffc27f12c305b2192fd44adcd3843b7ab67
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-29 21:55:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 board = ["   ", "X", "O"]
 
 def display_board(board)
-  puts "#{board[0]}   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
